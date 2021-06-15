05a070606a43ef80ec25ef950e40213afc525235
Pedro Wood <mail_pedro@yahoo.com.ar>
2021-06-15 10:37:38 -0300

    initial

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interviews.Improve
+{
+    public class Class1
+    {
+        public double Sum(List<double> values)
+        {
+            double sum = 0;
+            for(var i = 0; i < values.Count; i++)
+            {
+                sum += values[i];
+            }
+            return sum;
+        }
+
+        public double Sum(List<string> values)
+        {
+            double sum = 0;
+            for (var i = 0; i < values.Count; i++)
+            {
+                sum += Convert.ToDouble(values[i]);
+            }
+            return sum;
+        }
+    }
+}
---
+++ Improved.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interviews.Improve
+{
+    class Improved
+    {
+        public double Sum(IEnumerable<double> values)
+        {
+            return values.Sum();
+        }
+
+        public double Sum(IEnumerable<string> values)
+        {
+            double sum = 0;
+            foreach (var value in values)
+            {
+                if (double.TryParse(value, out double a))
+                    sum += a;
+            }
+            return sum;
+        }
+    }
+}
